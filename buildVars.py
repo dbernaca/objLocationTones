7276c39c9be9e35df19a7bf3903d5a534d9a242e
Joseph Lee <joseph.lee22590@gmail.com>
2017-05-27 13:56:01 -0700

    Build vars: Object Location Tones dev is born

--- buildVars.py
+++ buildVars.py
@@ -11,19 +11,19 @@ addon_info = {
 	# for previously unpublished addons, please follow the community guidelines at:
 	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
 	# add-on Name, internal for nvda
-	"addon_name" : "wintenApps",
+	"addon_name" : "objLocTones",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("Windows 10 App Essentials"),
+	"addon_summary" : _("Object Location Tones"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""A collection of app modules for various Windows 10 apps"""),
+	"addon_description" : _("""Announce location of objects via tones"""),
 	# version
-	"addon_version" : "17.06",
+	"addon_version" : "dev",
 	# Author(s)
-	"addon_author" : u"Joseph Lee <joseph.lee22590@gmail.com>, Derek Riemer <driemer.riemer@gmail.com> and others",
+	"addon_author" : u"Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url" : "http://addons.nvda-project.org/",
+	"addon_url" : "https://addons.nvda-project.org/",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 }
@@ -34,8 +34,7 @@ import os.path
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
 pythonSources = [os.path.join("addon", "*.py"),
-os.path.join("addon", "appModules", "*.py"),
-os.path.join("addon", "globalPlugins", "wintenObjs", "*.py")]
+os.path.join("addon", "globalPlugins", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]

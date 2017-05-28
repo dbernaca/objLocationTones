2bfd934276de3fb8e416252c0df0a7cae302c72b
Joseph Lee <joseph.lee22590@gmail.com>
2017-05-28 10:53:13 -0700

    ExecuteEvent: call the original first.
    Reviewed by Tyler Spievy: call the original function first in order to safeguard against possible corruptions.
    If an object is offscreen, tones will not be heard.

--- addon/globalPlugins/objloc.py
+++ addon/globalPlugins/objloc.py
@@ -1,5 +1,6 @@
 # Object Location Tones
-# A global plugin for NVDA# Copyright 2017 Joseph Lee, released under GPL
+# A global plugin for NVDA
+# Copyright 2017 Joseph Lee, released under GPL
 
 # Brings NVDA Core issue 2559 to life.
 
@@ -9,44 +10,34 @@ import tones
 import config
 import api
 import globalVars
-from logHandler import log
+
+# Safeguard against accidental overrides.
+_origExecuteEvent = eventHandler.executeEvent
 
 # Borrowed from NvDA Core's event handler code (credit: Nv Access)
 def executeEvent(eventName,obj,**kwargs):
-	"""Executes an NVDA event.
-	@param eventName: the name of the event type (e.g. 'gainFocus', 'nameChange')
-	@type eventName: string
-	@param obj: the object the event is for
-	@type obj: L{NVDAObjects.NVDAObject}
-	@param kwargs: Additional event parameters as keyword arguments.
-	"""
-	try:
-		sleepMode=obj.sleepMode
-		if eventName=="gainFocus" and not eventHandler.doPreGainFocus(obj,sleepMode=sleepMode):
-			return
-		elif not sleepMode and eventName=="documentLoadComplete" and not eventHandler.doPreDocumentLoadComplete(obj):
-			return
-		elif not sleepMode:
-			# JL/NVDA Core issue 2559: play object location tone if requested.
-			if eventName == "becomeNavigatorObject":
-				try:
-					l,t,w,h=obj.location
-					x = l+(w/2)
-					y = t+(h/2)
-					screenWidth, screenHeight = api.getDesktopObject().location[2:]
-					if x <= screenWidth or y <= screenHeight:
-						minPitch=config.conf['mouse']['audioCoordinates_minPitch']
-						maxPitch=config.conf['mouse']['audioCoordinates_maxPitch']
-						curPitch=minPitch+((maxPitch-minPitch)*((screenHeight-y)/float(screenHeight)))
-						brightness=config.conf['mouse']['audioCoordinates_maxVolume']
-						leftVolume=int((85*((screenWidth-float(x))/screenWidth))*brightness)
-						rightVolume=int((85*(float(x)/screenWidth))*brightness)
-						tones.beep(curPitch,40,left=leftVolume,right=rightVolume)
-				except:
-					pass
-			eventHandler._EventExecuter(eventName,obj,kwargs)
-	except:
-		log.exception("error executing event: %s on %s with extra args of %s"%(eventName,obj,kwargs))
+	# Call the original function first (suggested by Tyler Spivey).
+	# The only change is checking for sleep mode.
+	_origExecuteEvent(eventName,obj,**kwargs)
+	if obj.sleepMode: return
+	# JL/NVDA Core issue 2559: play object location tone if requested.
+	if eventName == "becomeNavigatorObject":
+		try:
+			l,t,w,h=obj.location
+			x = l+(w/2)
+			y = t+(h/2)
+			screenWidth, screenHeight = api.getDesktopObject().location[2:]
+			if 0 <= x <= screenWidth and 0 <= y <= screenHeight:
+				minPitch=config.conf['mouse']['audioCoordinates_minPitch']
+				maxPitch=config.conf['mouse']['audioCoordinates_maxPitch']
+				curPitch=minPitch+((maxPitch-minPitch)*((screenHeight-y)/float(screenHeight)))
+				brightness=config.conf['mouse']['audioCoordinates_maxVolume']
+				leftVolume=int((85*((screenWidth-float(x))/screenWidth))*brightness)
+				rightVolume=int((85*(float(x)/screenWidth))*brightness)
+				tones.beep(curPitch,40,left=leftVolume,right=rightVolume)
+		except:
+			pass
+
 
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
@@ -57,8 +48,11 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		if globalVars.appArgs.secure: return
 		# Until NVDA Core 2559 is implemented...
 		# There is a pull request for this issue. This pull request modifies the base becomeNavigatorObject event itself instead of replacing event handler function.
-		self._origExecuteEvent = eventHandler.executeEvent
+		global _origExecuteEvent
+		_origExecuteEvent = eventHandler.executeEvent
 		eventHandler.executeEvent = executeEvent
 
 	def terminate(self):
-		eventHandler.executeEvent = self._origExecuteEvent
+		global _origExecuteEvent
+		eventHandler.executeEvent = _origExecuteEvent
+		_origExecuteEvent = None

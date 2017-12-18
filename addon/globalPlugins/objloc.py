d3172ea8fac3b5e4fcf0b2a86d25013672a13c84
Joseph Lee <joseph.lee22590@gmail.com>
2017-12-18 09:05:10 -0800

    Use become nav object event instead of patching eventHandler.executeEvent.
    Global plugin objects are, in fact, scriptable object, which permits event execution and thus can accept 'becomeNavigatorObject' event. With this in mind, use that event instead of patching eventHandler.executeEvent.

--- addon/globalPlugins/objloc.py
+++ addon/globalPlugins/objloc.py
@@ -5,23 +5,20 @@
 # Brings NVDA Core issue 2559 to life.
 
 import globalPluginHandler
-import eventHandler
 import tones
 import config
 import api
 import globalVars
 
-# Safeguard against accidental overrides.
-_origExecuteEvent = eventHandler.executeEvent
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
-# Borrowed from NvDA Core's event handler code (credit: Nv Access)
-def executeEvent(eventName,obj,**kwargs):
-	# Call the original function first (suggested by Tyler Spivey).
-	# The only change is checking for sleep mode.
-	_origExecuteEvent(eventName,obj,**kwargs)
-	if obj.sleepMode: return
-	# JL/NVDA Core issue 2559: play object location tone if requested.
-	if eventName == "becomeNavigatorObject":
+	def __init__(self):
+		super(globalPluginHandler.GlobalPlugin, self).__init__()
+		# No, I'll never let you announce obj coords in secure screens.
+		if globalVars.appArgs.secure: return
+
+	def event_becomeNavigatorObject(self, obj, nextHandler):
+		# Until NVDA Core 2559 is implemented...
 		try:
 			l,t,w,h=obj.location
 			x = l+(w/2)
@@ -37,22 +34,3 @@ def executeEvent(eventName,obj,**kwargs):
 				tones.beep(curPitch,40,left=leftVolume,right=rightVolume)
 		except:
 			pass
-
-
-
-class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-
-	def __init__(self):
-		super(globalPluginHandler.GlobalPlugin, self).__init__()
-		# No, I'll never let you announce obj coords in secure screens.
-		if globalVars.appArgs.secure: return
-		# Until NVDA Core 2559 is implemented...
-		# There is a pull request for this issue. This pull request modifies the base becomeNavigatorObject event itself instead of replacing event handler function.
-		global _origExecuteEvent
-		_origExecuteEvent = eventHandler.executeEvent
-		eventHandler.executeEvent = executeEvent
-
-	def terminate(self):
-		global _origExecuteEvent
-		eventHandler.executeEvent = _origExecuteEvent
-		_origExecuteEvent = None

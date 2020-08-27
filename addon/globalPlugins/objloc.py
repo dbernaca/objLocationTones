a1ae71244d89c0619a9637ae3c20dde3fa9eb042
Joseph Lee <joseph.lee22590@gmail.com>
2020-08-26 20:56:49 -0700

    Flake8 E226: add space around arithmetic operator

--- addon/globalPlugins/objloc.py
+++ addon/globalPlugins/objloc.py
@@ -24,16 +24,16 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		# Until NVDA Core 2559 is implemented...
 		try:
 			l, t, w, h = obj.location
-			x = l+(w/2)
-			y = t+(h/2)
+			x = l + (w / 2)
+			y = t + (h / 2)
 			screenWidth, screenHeight = api.getDesktopObject().location[2:]
 			if 0 <= x <= screenWidth and 0 <= y <= screenHeight:
 				minPitch = config.conf['mouse']['audioCoordinates_minPitch']
 				maxPitch = config.conf['mouse']['audioCoordinates_maxPitch']
-				curPitch = minPitch+((maxPitch-minPitch)*((screenHeight-y)/float(screenHeight)))
+				curPitch = minPitch + ((maxPitch - minPitch) * ((screenHeight - y) / float(screenHeight)))
 				brightness = config.conf['mouse']['audioCoordinates_maxVolume']
-				leftVolume = int((85*((screenWidth-float(x))/screenWidth))*brightness)
-				rightVolume = int((85*(float(x)/screenWidth))*brightness)
+				leftVolume = int((85 * ((screenWidth - float(x)) / screenWidth)) * brightness)
+				rightVolume = int((85 * (float(x) / screenWidth)) * brightness)
 				tones.beep(curPitch, 40, left=leftVolume, right=rightVolume)
 		except:
 			pass

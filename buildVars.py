c6145d36cacbe92f5b76907d4f4a0a43250afbb4
Joseph Lee <joseph.lee22590@gmail.com>
2020-11-05 10:12:01 -0800

    Build vars: update syntax.

--- buildVars.py
+++ buildVars.py
@@ -27,15 +27,15 @@ addon_info = {
 	# version
 	"addon_version": "20.09",
 	# Author(s)
-	"addon_author": u"Joseph Lee <joseph.lee22590@gmail.com>",
+	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support
 	"addon_url": "https://addons.nvda-project.org/",
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2019.3.0",
+	"addon_minimumNVDAVersion": "2019.3",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2020.3.0",
+	"addon_lastTestedNVDAVersion": "2020.3",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!

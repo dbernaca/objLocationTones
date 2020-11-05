1463920ffe776966207efc75b1512791bf44635c
Joseph Lee <joseph.lee22590@gmail.com>
2020-11-05 10:13:25 -0800

    Flake8 ET122: indent python sources list

--- buildVars.py
+++ buildVars.py
@@ -53,8 +53,10 @@ import os.path  # NOQA
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = [os.path.join("addon", "*.py"),
-os.path.join("addon", "globalPlugins", "*.py")]
+pythonSources = [
+	os.path.join("addon", "*.py"),
+	os.path.join("addon", "globalPlugins", "*.py")
+]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]

31a537bcbac2d1bb70ca6b49847acda993d79e31
Joseph Lee <joseph.lee22590@gmail.com>
2020-11-22 22:55:39 -0800

    Object Location Tones 20.12
    Signed-off-by: Joseph Lee <joseph.lee22590@gmail.com>

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Announce location of objects via tones"""),
 	# version
-	"addon_version": "20.09",
+	"addon_version": "20.12",
 	# Author(s)
 	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support

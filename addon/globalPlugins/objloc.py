77cabc836fe8089edaed477250c9f7c22116f9c5
Dalen <dbernaca@gmail.com>
2024-05-03 02:38:05 +0200

    Added support for focused object's outline reporting on demand. You can use an unassigned input gesture to play all 4 corner positions for the currently focused object. This helps in locating the object, along with getting perception of its size.
    Expanded BBox() attributes and capabilities
    
    Added __doc__ strings and comments
    
    Renamed one input gesture description
    
    Fixed some minor bugs
    
    Returned wx.CallAfter() in _on_mouseMonitor()

--- addon/globalPlugins/objloc.py
+++ addon/globalPlugins/objloc.py
@@ -43,6 +43,18 @@ def isEditable (obj):
     return r==ROLE_EDITABLETEXT or r==ROLE_PASSWORDEDIT or r==ROLE_TERMINAL or r==ROLE_DOCUMENT
 
 def getObjectPos (obj=None, location=True, caret=False):
+    """
+    Returns x and y coordinates of the obj.
+    The obj argument is an object you wish to get the position for, or None (default).
+    If None, api.getFocusObject() is used to get the object to use.
+    If caret argument is True (defaults to False), function will return position of the caret
+    in case the obj is considered editable. If location is False (defaults to True),
+    and caret position is unavailable, then the centroid location of the editable
+    will be returned instead. In all other circumstances
+    the coordinates x, y of the center of mass for the
+    obj will be returned, and, if not available, LocationError()
+    will be raised.
+    """
     try:
         obj = obj or getFocusObject()
         if caret:
@@ -61,43 +73,74 @@ def getObjectPos (obj=None, location=True, caret=False):
     raise LocationError("Location unavailable")
 
 class BBox (object):
-    __slots__ = ("L", "T", "W", "H", "X1", "X2", "X3", "X4", "Y1", "Y2", "Y3", "Y4")
+    """
+    Class for dealing with more complex information of object locations.
+    And providing potential operations regarding the same.
+    """
+    __slots__ = ("L", "T", "W", "H", "X1", "X2", "X3", "X4", "Y1", "Y2", "Y3", "Y4", "TL", "TR", "BR", "BL", "corners")
     def __init__ (self, obj):
+        """
+        Takes an object and initializes its bounding box.
+        """
         loc = obj.location
         self.L, self.T, self.W, self.H = loc
-        self.X1 = loc[0]
-        self.Y1 = loc[1]
-        self.X2 = loc[0]+loc[2]
-        self.Y2 = loc[1]
-        self.X3 = loc[0]+loc[2]
-        self.Y3 = loc[1]+loc[3]
-        self.X4 = loc[0]
-        self.Y4 = loc[1]+loc[3]
+        # Left top corner
+        self.X1 = x1 = loc[0]
+        self.Y1 = y1 = loc[1]
+        self.TL = (x1, y1)
+        # Right top corner
+        self.X2 = x2 = loc[0]+loc[2]
+        self.Y2 = y2 = loc[1]
+        self.TR = (x2, y2)
+        # Right bottom corner
+        self.X3 = x3 = loc[0]+loc[2]
+        self.Y3 = y3 = loc[1]+loc[3]
+        self.BR = (x3, y3)
+        # Left Bottom corner
+        self.X4 = x4 = loc[0]
+        self.Y4 = y4 = loc[1]+loc[3]
+        self.BL = (x4, y4)
+        self.corners = ((x1, y1), (x2, y2), (x3, y3), (x4, y4))
 
-    def __contains__ (self, xy):
-        return (self.X1 <= xy[0] <= self.X2) and (self.Y1 <= xy[1] <= self.Y4)
+    def __contains__ (self, obj):
+        """
+        Checks whether a point (x, y) or a BBox() of an obj or obj itself given by argument obj,
+        occupies the space bounded by this bounding box.
+        Returns True if this BBox() and the operand obj share any points between them,
+        False otherwise. Intended use is, for example:
+        (10, 10) in BBox(obj)
+        """
+        if isinstance(obj, tuple):
+            return (self.X1 <= obj[0] <= self.X2) and (self.Y1 <= obj[1] <= self.Y4)
+        obj = obj if isinstance(obj, BBox) else BBox(obj)
+        return any((xy in self) for xy in obj.corners)
 
 class GlobalPlugin (globalPluginHandler.GlobalPlugin):
     def __init__ (self):
         super(globalPluginHandler.GlobalPlugin, self).__init__()
 
-        self.duration   = 40
-        self.volume     = maxVolume
-        self.stereoSwap = False
-        self.tolerance  = 20
-        self.timeout    = 2
-        self.caret      = True
+        self.duration   = 40 # Duration of a positional tone in Msec
+        self.volume     = maxVolume # Volume of positional tones in percents
+        self.stereoSwap = False # Swap stereo sides
+        self.tolerance  = 20 # Mouse to location arrivall detection tolerance
+                             # distance between two points in pixels
+        self.timeout    = 2 # Timeout to stop the mouse monitoring (in seconds)
+        self.caret      = True # Whether to report caret location for editable fields
 
         self.focusing     = True # A flag to prevent double beeps on focus of text area children
-        self.entered      = False
+        self.entered      = False # A flag for reporting entering and exiting of the focused object area
+
         self.lastMousePos = (-1, -1) # Used to detect that the mouse stopped moving so that we can stop the timer
         self.lastTime     = 0.0 # Used to detect how much time passed while mouse is stopped
 
+        # Mouse monitoring position playing timer
         self.timer = wx.Timer(gui.mainFrame, wx.ID_ANY)
         gui.mainFrame.Bind(wx.EVT_TIMER, self._on_mouseMonitor, self.timer)
 
+        # Initial NVDA event bindings
         self.event_becomeNavigatorObject = self._on_becomeNavigatorObject
         self.event_caret = self._on_caret
+        self.event_mouseMove = self._on_passThrough
 
     def terminate (self):
         self.timer.Stop()
@@ -120,11 +163,43 @@ class GlobalPlugin (globalPluginHandler.GlobalPlugin):
             beep(curPitch, (d or self.duration), left=leftVolume, right=rightVolume)
 
     @scriptHandler.script(
+        # Translators: Object outline report gesture description in the input gesture dialog
+        description=_("Report focused object's outline via positional tones."),
+        # Translators: Input gestures dialog category for objLocTones.
+        category=_("Object Location Tones") )
+    def script_objectOutline (self, gesture):
+        """
+        Plays positional tones for all 4 corners of the object's bounding box.
+        If the object is editable, adds final tone for the caret position.
+        """
+        try:
+            obj = getFocusObject()
+            rect = BBox(obj)
+            wx.CallAfter(self.playCoordinates, rect.X1, rect.Y1, self.duration+20)
+            wx.CallLater(self.duration+220, self.playCoordinates, rect.X2, rect.Y2, self.duration+20)
+            wx.CallLater(self.duration+620, self.playCoordinates, rect.X3, rect.Y3, self.duration+20)
+            wx.CallLater(self.duration+820, self.playCoordinates, rect.X4, rect.Y4, self.duration+20)
+            try:
+                if not isEditable(obj):
+                    return
+                oX, oY = getObjectPos(obj, location=False, caret=True)
+                wx.CallLater(self.duration+1120, self.playCoordinates, oX, oY, self.duration+150)
+            except:
+                pass
+        except:
+            ui.message(_("Location unavailable"))
+
+    @scriptHandler.script(
         # Translators: The toggle mouse location monitoring gesture description in the input gesture dialog
         description=_("Toggle a mouse cursor position in relation to navigator object location reporting via positional tones."),
         # Translators: Input gestures dialog category for objLocTones.
         category=_("Object Location Tones") )
     def script_toggleMouseMonitor (self, gesture):
+        """
+        Activates or deactivates real-time mouse location monitoring.
+        The location is presented in relation to the focused object's centroid
+        or the caret position within an editable field.
+        """
         if not self.timer.IsRunning():
             try:
                 oX, oY = getObjectPos(caret=self.caret)
@@ -168,12 +243,12 @@ class GlobalPlugin (globalPluginHandler.GlobalPlugin):
 
     @scriptHandler.script(
         # Translators: The gesture description for on request object location in the input gesture dialog
-        description=_("Play a positional tone for the current navigator object"),
+        description=_("Play a positional tone for currently focused object"),
         # Translators: Input gestures dialog category for objLocTones.
         category=_("Object Location Tones") )
     def script_locate (self, gesture):
         """
-        Plays positional tone for the current navigator object location on demand
+        Plays positional tone for currently focused object location on demand
         """
         self.timer.Stop()
         try:
@@ -202,6 +277,9 @@ class GlobalPlugin (globalPluginHandler.GlobalPlugin):
         self.event_becomeNavigatorObject = self._on_passThrough
         self.event_caret = self._on_passThrough
         self.timer.Stop()
+        self.event_mouseMove = self._on_passThrough
+        self.lastMousePos = (-1, -1)
+        self.lastTime = 0.0
         # Translators: Message when positional tones are switched off
         ui.message(_("Positional tones off"))
 
@@ -263,6 +341,7 @@ class GlobalPlugin (globalPluginHandler.GlobalPlugin):
             self.event_mouseMove = self._on_passThrough
             ui.message(_("Location unavailable"))
             return
+        # If mouse is stationary for too long, automatically stop monitoring:
         t   = time()
         lmp = self.lastMousePos
         if lmp==mp and t-self.lastTime>=self.timeout:
@@ -275,10 +354,16 @@ class GlobalPlugin (globalPluginHandler.GlobalPlugin):
         if lmp!=mp:
             self.lastMousePos = mp
             self.lastTime = t
-        self.playCoordinates(mp[0], mp[1], self.duration+40)
+        wx.CallAfter(self.playCoordinates, mp[0], mp[1], self.duration+40)
         wx.CallLater(self.duration+100, self.playCoordinates, oX, oY, self.duration+70)
 
     def _on_mouseMove (self, obj, nextHandler, x, y):
+        """
+        Event used during mouse monitoring that checks for the current
+        location of mouse cursor in relation to focused object or caret position.
+        If cursor is in tolerated distance, the hit is reported and monitoring ends.
+        The event also reports entering and exiting the focused object.
+        """
         try:
             fobj = getFocusObject()
             oX, oY = getObjectPos(fobj, caret=self.caret)
@@ -309,8 +394,6 @@ class GlobalPlugin (globalPluginHandler.GlobalPlugin):
             self.lastTime     = 0.0
             speech.cancelSpeech()
             ui.message(_("Location reached"))
-            nextHandler()
-            return
         nextHandler()
 
     event_mouseMove = _on_passThrough
